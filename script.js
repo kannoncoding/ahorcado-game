d326a9f73932a461de45aa88f7d081aa5a29b258
kannoncoding <codingadaptations@gmail.com>
2025-04-20 14:39:10 -0600

    initial files structure established

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnRegistrar = document.getElementById("btnRegistrar");
+    const btnIniciarJuego = document.getElementById("btnIniciarJuego");
+    const btnIntentar = document.getElementById("btnIntentar");
+  
+    btnRegistrar.addEventListener("click", () => {
+      // Próximamente: registrar jugador
+    });
+  
+    btnIniciarJuego.addEventListener("click", () => {
+      // Próximamente: iniciar juego con categoría
+    });
+  
+    btnIntentar.addEventListener("click", () => {
+      // Próximamente: lógica de letras
+    });
+  });
+  
